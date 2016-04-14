11de5d66c809c2f542905741824d6799ab6929a9
Tamas Kalman <t.kalman@prezi.com>
2016-04-14 16:12:14 -0700

    repo cloning added

--- tg.php
+++ tg.php
@@ -5,3 +5,29 @@
 // Author: Tamas Kalman <ktamas77@gmail.com>
 // Web: https://github.com/ktamas77/team-gource
 
+$config = yaml_parse(file_get_contents('tg.conf'));
+
+function updateRepo($org, $repo) {
+    if (!is_dir('repos')) {
+        mkdir('repos');
+    }
+    exec("git clone git@github.com:$org/$repo.git repos/$repo");
+}
+
+$teams = $config['teams'];
+foreach ($teams as $team) {
+    $teamName = $team['name'];
+    $teamCollection = $team['collection'];
+    print "Team: $teamName\n";
+    print "Collection: $teamCollection\n";
+    $repos = $config['collections'];
+    foreach ($repos as $repo) {
+        if ($repo['name'] === $teamCollection) {
+            $teamOrg = $repo['organization'];
+            $teamRepos = $repo['repos'];
+            foreach ($teamRepos as $repoName) {
+                updateRepo($teamOrg, $repoName);
+            }
+        }
+    }
+}
